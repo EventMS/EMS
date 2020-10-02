0aef07e3ccbb1a16467f4e3fe7dbb9eea1c87cd9
Mads Skytte Nielsen <au536099@uni.au.dk>
2020-10-02 11:32:20 +0200

    Update requirements

--- src/Services/Club/Club.API/GraphQlQueries/Request/UpdateTemplate1Request.cs
+++ src/Services/Club/Club.API/GraphQlQueries/Request/UpdateTemplate1Request.cs
@@ -1,15 +1,30 @@
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Club.API.Controllers.Request
 {
     public class UpdateClubRequest
     {
+        [Required]
+        [MaxLength(25)]
         public string Name { get; set; }
+        [MaxLength(500)]
         public string Description { get; set; }
+        [Required]
+        [MaxLength(8)]
+        [MinLength(8)]
         public string PhoneNumber { get; set; }
+        [Required]
+        [MaxLength(4)]
+        [MinLength(4)]
         public string RegistrationNumber { get; set; }
+        [Required]
+        [MaxLength(8)]
+        [MinLength(8)]
         public string AccountNumber { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Address { get; set; }
     }
 }
