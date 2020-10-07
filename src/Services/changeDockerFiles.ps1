fc98aeedb7326a6ee3dccd19673795f0751ec01a
madssn <mads.nielsen.skytte@gmail.com>
2020-10-07 13:42:26 +0200

    Changed auto generator file

--- src/Services/changeDockerFiles.ps1
+++ src/Services/changeDockerFiles.ps1
@@ -7,3 +7,9 @@ Foreach($file in $files){
 	(Get-Content $file.FullName).replace('COPY "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj" "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj"', 'COPY "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj" "Services/'+$from+'/'+$from+'.API/'+$from+'.API.csproj"
 	COPY "Services/'+$to+'/'+$to+'.API/'+$to+'.API.csproj" "Services/'+$to+'/'+$to+'.API/'+$to+'.API.csproj"') | Set-Content $file.FullName
 }
+
+
+Foreach($file in $files){
+	(Get-Content $file.FullName).replace('COPY "Services/'+$from+'/'+$from+'.API.UnitTests/'+$from+'.API.UnitTests.csproj" "Services/'+$from+'/'+$from+'.API.UnitTests/'+$from+'.API.UnitTests.csproj"', 'COPY "Services/'+$from+'/'+$from+'.API.UnitTests/'+$from+'.API.UnitTests.csproj" "Services/'+$from+'/'+$from+'.API.UnitTests/'+$from+'.API.UnitTests.csproj"
+	COPY "Services/'+$to+'/'+$to+'.API.UnitTests/'+$to+'.API.UnitTests.csproj" "Services/'+$to+'/'+$to+'.API.UnitTests/'+$to+'.API.UnitTests.csproj"') | Set-Content $file.FullName
+}
