d9090dd2f3d4333c66120ec27e67ae43fcb66d5c
Mads Skytte Nielsen <au536099@uni.au.dk>
2020-10-21 16:20:57 +0200

    ..

--- src/Services/Subscription/Subscription.API/GraphQlQueries/Request/CreateSubscriptionRequest.cs
+++ src/Services/Subscription/Subscription.API/GraphQlQueries/Request/CreateSubscriptionRequest.cs
@@ -1,8 +1,10 @@
-﻿namespace EMS.Subscription_Services.API.GraphQlQueries.Request
+﻿using System;
+
+namespace EMS.Subscription_Services.API.GraphQlQueries.Request
 {
     public class CreateSubscriptionRequest
     {
         public string PaymentMethodId { get; set; }
-        public string ClubSubscriptonId { get; set; }
+        public Guid ClubSubscriptonId { get; set; }
     }
 }
