6d45660e63337207c55d784c098b978d5fb5f3a2
jepstroem <repsej.stroem@gmail.com>
2020-10-23 13:37:02 +0200

    Permissions for user added

--- src/Services/Permission/Permission.API/GraphQlQueries/PermissionQueries.cs
+++ src/Services/Permission/Permission.API/GraphQlQueries/PermissionQueries.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using EMS.Club_Service_Services.API;
 using EMS.Permission_Services.API.Context;
@@ -16,6 +17,7 @@ namespace EMS.Permission_Services.API.GraphQlQueries
 
         public IQueryable<User> Permissions => _context.Users.Include(u => u.Roles).AsQueryable();
 
+        public IQueryable<Role> RolesForUserId(Guid userId) => _context.Roles.Where(role => role.UserId == userId).AsQueryable();
 
         public IQueryable<Role> UserRoles([CurrentUserGlobalState] CurrentUser user) => _context.Roles.Where(role => role.UserId == user.UserId).AsQueryable();
     }
