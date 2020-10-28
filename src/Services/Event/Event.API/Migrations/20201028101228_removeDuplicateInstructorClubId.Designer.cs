b043c47eb27fc623a0d9180f2efce11669425889
madssn <mads.nielsen.skytte@gmail.com>
2020-10-28 11:18:20 +0100

    Updated event service mgiration

--- src/Services/Event/Event.API/Migrations/20201028101228_removeDuplicateInstructorClubId.Designer.cs
+++ src/Services/Event/Event.API/Migrations/20201028101228_removeDuplicateInstructorClubId.Designer.cs
@@ -4,14 +4,16 @@ using EMS.Event_Services.API.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EMS.Event_Services.API.Migrations
 {
     [DbContext(typeof(EventContext))]
-    partial class EventContextModelSnapshot : ModelSnapshot
+    [Migration("20201028101228_removeDuplicateInstructorClubId")]
+    partial class removeDuplicateInstructorClubId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/Event/Event.API/Migrations/20201028101228_removeDuplicateInstructorClubId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EMS.Event_Services.API.Migrations
+{
+    public partial class removeDuplicateInstructorClubId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Instructor_Club_ClubId1",
+                table: "Instructor");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Instructor_ClubId1",
+                table: "Instructor");
+
+            migrationBuilder.DropColumn(
+                name: "ClubId1",
+                table: "Instructor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ClubId1",
+                table: "Instructor",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instructor_ClubId1",
+                table: "Instructor",
+                column: "ClubId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Instructor_Club_ClubId1",
+                table: "Instructor",
+                column: "ClubId1",
+                principalTable: "Club",
+                principalColumn: "ClubId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
