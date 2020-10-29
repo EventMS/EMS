089a3357fe664499bc3fc3f2573f5572fe6cfd3e
Simon Wessmann <simon.wessmann.brok@gmail.com>
2020-10-29 17:01:43 +0100

    eventVerificationQuery

--- src/Services/EventVerification/EventVerification.API/GraphQlQueries/EventVerificationQueries.cs
+++ src/Services/EventVerification/EventVerification.API/GraphQlQueries/EventVerificationQueries.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using EMS.EventVerification_Services.API.Context;
 using EMS.EventVerification_Services.API.Context.Model;
 
@@ -13,5 +15,8 @@ namespace EMS.EventVerification_Services.API.GraphQlQueries
         }
 
         public IQueryable<EventVerification> EventVerifications => _context.EventVerifications.AsQueryable();
+
+        public IQueryable<EventVerification> EventsForUser(Guid userId) => _context.EventVerifications.Where(eventVerification => eventVerification.UserId == userId).AsQueryable();
+
     }
 }
