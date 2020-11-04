6a2633f82e272713e2c9be41e9e9040ca1293fe7
madssn <mads.nielsen.skytte@gmail.com>
2020-11-04 17:21:52 +0100

    Small adjustments

--- deploy/kubernetes/kube-teardown-services.ps1
+++ deploy/kubernetes/kube-teardown-services.ps1
@@ -3,7 +3,7 @@ kubectl config set-context --current --namespace=eventms;
 
 #Delete all resources
 # kubectl delete -f .\Database\.; # Uncomment if needed
-Get-ChildItem -Directory -Filter *Service | ForEach-Object {kubectl apply -f .\$_\.;}
+Get-ChildItem -Directory -Filter *Service | ForEach-Object {kubectl delete -f .\$_\.;}
 # kubectl delete -f .\Ingress\.; # No need to delete the ingress always as it takes forever to get it up again
 
 
