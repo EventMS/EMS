fbaa211bd7268aa5d14334e72b6261ec06e2f8ea
madssn <mads.nielsen.skytte@gmail.com>
2020-11-04 11:40:08 +0100

    More temp

--- deploy/kubernetes/changeMapping.ps1
+++ deploy/kubernetes/changeMapping.ps1
@@ -1,11 +1,10 @@
-$from=$args[0]
-$to=$args[1]
+$serviceName=$args[0]
+$folderName=$serviceName + "Service"
+$serviceName = $serviceName.substring(0,1).toLower() + $serviceName.substring(1)
 
-Get-ChildItem -Path $to -Recurse -Include *.yaml | Rename-Item -NewName { $_.Name.replace($from,$to)} #Files
-Get-ChildItem -Path $to -Recurse -Include * | Rename-Item -NewName { $_.Name.replace($from,$to)} #Directories and rest..
+Get-ChildItem -Path $folderName -Recurse -Include *.yaml | Rename-Item -NewName { $_.Name.replace("template1",$serviceName)} #Files
 
-$files = Get-ChildItem -Path $to -Recurse -Include *.yaml
-
+$files = Get-ChildItem -Path $folderName -Recurse -Include *.yaml
 Foreach($file in $files){
-	(Get-Content $file.FullName).replace($from, $to) | Set-Content $file.FullName
+	(Get-Content $file.FullName).replace("template1", $serviceName) | Set-Content $file.FullName
 }
--- deploy/kubernetes/generateServices.ps1
+++ deploy/kubernetes/generateServices.ps1
@@ -1,9 +1,9 @@
 $from=$args[0]
 $to=$args[1]
 
-Get-ChildItem -Path .\..\..\src\Services -Directory | ForEach-Object {
-$_
-#& .\copy.ps1 $from $to
-#& .\changeMapping.ps1 $from $to
+Get-ChildItem -Path .\..\..\src\Services -Directory -Exclude Templa* | ForEach-Object {
+$ServiceName = $_.Name + "Service"
+& .\copy.ps1 Template1Service $ServiceName
+& .\changeMapping.ps1 $_.Name
 }
 
