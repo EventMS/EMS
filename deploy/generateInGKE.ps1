9cc94996150a19af20018f7e2e163b6486fd0f5b
madssn <mads.nielsen.skytte@gmail.com>
2020-11-11 21:43:11 +0100

    Small stuff

--- deploy/generateInGKE.ps1
+++ deploy/generateInGKE.ps1
@@ -1,9 +1,13 @@
 $ProjectName = "dark-balancer-294711"
+gcloud builds submit --project=$ProjectName --config gke-namespaces
+gcloud builds submit --project=$ProjectName --config gke-messagequeue
+gcloud builds submit --project=$ProjectName --config gke-frontendservice
 gcloud builds submit --project=$ProjectName --config gke-clubservice
 gcloud builds submit --project=$ProjectName --config gke-clubmemberservice
 gcloud builds submit --project=$ProjectName --config gke-eventparticipantservice
 gcloud builds submit --project=$ProjectName --config gke-eventservice
 gcloud builds submit --project=$ProjectName --config gke-eventverificationservice
+gcloud builds submit --project=$ProjectName --config gke-graphqlservice
 gcloud builds submit --project=$ProjectName --config gke-identityservice
 gcloud builds submit --project=$ProjectName --config gke-paymentservice
 gcloud builds submit --project=$ProjectName --config gke-paymentwebhookservice
